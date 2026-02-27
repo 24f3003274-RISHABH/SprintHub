dea39941c7dac37b1f0f41a881f0d950e73c4a23
24f3003274 <24f3003274@ds.study.iitm.ac.in>
2026-02-27 23:29:06 +0530

    comment out

--- server/server.js
+++ server/server.js
@@ -11,3 +11,4 @@ app.get('/',(req,res)=>res.send("Server is running..."));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT,()=>console.log(`Server is running on port ${PORT}`));
+// #server will run on port 5000 or the port specified in the environment variables.
